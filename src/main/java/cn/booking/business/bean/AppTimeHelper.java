164cc9aa0e9faa17ea16c9df3485f42ce7651d9e
qinyongbo <qinyongbo@chudaokeji.com>
2017-07-12 14:45:30 +0800

    add 基本接口

---
+++ src/main/java/cn/booking/business/bean/AppTimeHelper.java
@@ -0,0 +1,50 @@
+package cn.booking.business.bean;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+
+import cn.sdk.bean.BaseBean;
+/**
+ * 时间段
+ * @author Mbenben
+ *
+ */
+public class AppTimeHelper extends BaseBean implements Serializable {
+	/**
+	 * 预约时间段
+	 */
+	private String apptime;
+	/**
+	 * 最大预约数
+	 */
+	private String maxnumber;
+	/**
+	 * 已预约数
+	 */
+	private String yetnumber;
+	
+	public String getApptime() {
+		return apptime;
+	}
+	public void setApptime(String apptime) {
+		this.apptime = apptime;
+	}
+	public String getMaxnumber() {
+		return maxnumber;
+	}
+	public void setMaxnumber(String maxnumber) {
+		this.maxnumber = maxnumber;
+	}
+	public String getYetnumber() {
+		return yetnumber;
+	}
+	public void setYetnumber(String yetnumber) {
+		this.yetnumber = yetnumber;
+	}
+	
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this);
+	}
+}
--- src/main/java/cn/booking/business/service/IBookingBusinessService.java
+++ src/main/java/cn/booking/business/service/IBookingBusinessService.java
@@ -1,7 +1,9 @@
 package cn.booking.business.service;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
+import cn.booking.business.bean.AppTimeHelper;
 import cn.booking.business.bean.BusinessTypeVO;
 import cn.booking.business.bean.IdTypeVO;
 import cn.booking.business.bean.OrgVO;
@@ -46,4 +48,27 @@ public interface IBookingBusinessService{
 	 * @throws Exception
 	 */
 	public OrgVO getOrgsByBusinessTypeId(String btId,String arg0,String arg1) throws Exception;
+	/**
+	 * 获取可预约时间段
+	 * @param date 预约日期    不能为空
+	 * @param orgId 预约单位ID  不能为空
+	 * @param businessTypeId 业务类型ID
+	 * @param carTypeId 车辆类型ID
+	 * @param optlittleCar 车辆产地  ‘0’ 进口  ‘1’ 国产
+	 * @return
+	 * @throws Exception
+	 */
+	public List<AppTimeHelper> getAppTimes(String date,String orgId,String businessTypeId,String carTypeId,String optlittleCar)throws Exception;
+	/**
+	 * 获取可预约日期 
+	 * @param orgId 预约单位ID  必填
+	 * @param businessTypeId 业务类型ID 必填
+	 * @param arg0
+	 * @param arg1
+	 * @return
+	 * @throws Exception
+	 */
+	public List<String> getAppointmentDate(String orgId,String businessTypeId,String arg0,String arg1)throws Exception;
+	
+	
 }
