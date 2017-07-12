5a098275b60c66a4f812ef9732068eb8cf2c877c
qinyongbo <qinyongbo@chudaokeji.com>
2017-07-12 20:14:33 +0800

    add

---
+++ src/main/java/cn/booking/business/bean/SmsInfoVO.java
@@ -0,0 +1,41 @@
+package cn.booking.business.bean;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+
+public class SmsInfoVO implements Serializable {
+
+	private String code;
+	private String msg;
+	private String result;
+	
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this);
+	}
+}
--- src/main/java/cn/booking/business/service/IBookingBusinessService.java
+++ src/main/java/cn/booking/business/service/IBookingBusinessService.java
@@ -8,6 +8,7 @@ import cn.booking.business.bean.BusinessTypeVO;
 import cn.booking.business.bean.CarTypeVO;
 import cn.booking.business.bean.IdTypeVO;
 import cn.booking.business.bean.OrgVO;
+import cn.booking.business.bean.SmsInfoVO;
 
 public interface IBookingBusinessService{
 	/**
@@ -70,6 +71,19 @@ public interface IBookingBusinessService{
 	 * @throws Exception
 	 */
 	public List<String> getAppointmentDate(String orgId,String businessTypeId,String arg0,String arg1)throws Exception;
-	
-	
+	/**
+	 * 发送短信验证码 
+	 * @param mobile 获取短信验证码的手机号
+	 * @param idType 本次预约所填写的证件种类ID,由于六年免检没有证件种类，请传”0”
+	 * @param lx 1:驾驶证业务  2:机动车业务（六年免检业务传3）  3:其他（包括六年免检业务）
+	 * @param ip 用户客户端IP
+	 * @param bookerType ‘0’非代办（或本人） ‘1’普通代办 ‘2’专业代办（企业代办）
+	 * @param bookerName 代办人姓名： 0’非代办（或本人）情况请传本次预约业务填写的姓名
+	 * @param bookerIdNumber 代办人身份证号码： 0’非代办（或本人）情况请传本次预约业务填写的证件号码
+	 * @param idNumber 本次预约业务填写的证件号码
+	 * @param codes 本次预约的具体业务类型（例如：JD01）
+	 * @return
+	 * @throws Exception
+	 */
+	public SmsInfoVO simpleSendMessage(String mobile,String idType,String lx,String ip,String bookerType,String bookerName,String bookerIdNumber,String idNumber,String codes)throws Exception;
 }
